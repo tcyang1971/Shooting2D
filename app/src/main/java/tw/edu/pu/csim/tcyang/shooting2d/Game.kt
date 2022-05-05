6c35f494e66282b5444d21fb7f3f283b093d78e1
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-05-06 07:43:50 +0800

    v.2 SurfaceView遊戲背景

---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/shooting2d/Game.kt
@@ -0,0 +1,45 @@
+package tw.edu.pu.csim.tcyang.shooting2d
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class Game(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+    SurfaceHolder.Callback{
+
+    var surfaceHolder: SurfaceHolder
+    var BG: Bitmap
+
+    init {
+        surfaceHolder = getHolder()
+        BG = BitmapFactory.decodeResource(getResources(), R.drawable.background)
+        surfaceHolder.addCallback(this)
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+        var canvas: Canvas = surfaceHolder.lockCanvas()
+            drawSomething(canvas)
+        surfaceHolder.unlockCanvasAndPost(canvas)
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+
+    }
+
+    fun drawSomething(canvas:Canvas) {
+        var SrcRect: Rect = Rect(0, 0, BG.width, BG.height) //裁切
+        var w:Int = width
+        var h:Int = height
+        var DestRect:Rect = Rect(0, 0, w, h)
+        canvas.drawBitmap(BG, SrcRect, DestRect, null)
+    }
+}
