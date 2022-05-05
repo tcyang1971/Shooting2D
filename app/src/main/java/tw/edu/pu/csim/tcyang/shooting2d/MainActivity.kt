f8624473719dea6bbd17e903b7c5cef5b97314e5
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-05-06 07:51:15 +0800

    v.3 開始及結束圖形按鈕

--- app/src/main/java/tw/edu/pu/csim/tcyang/shooting2d/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/shooting2d/MainActivity.kt
@@ -2,10 +2,30 @@ package tw.edu.pu.csim.tcyang.shooting2d
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img : ImageView
+    lateinit var game : Game
+    var flag:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        img = findViewById(R.id.img)
+        game = findViewById(R.id.game)
+
+        img.setOnClickListener({
+            if (flag){
+                flag = false
+                img.setImageResource(R.drawable.start)
+            }
+            else{
+                flag = true
+                img.setImageResource(R.drawable.stop)
+            }
+
+        })
     }
 }
